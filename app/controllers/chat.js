a65bf723fa417c15b5ad0141d8f55f30bb0b5222
aqSelmo <guilusa25@gmail.com>
2019-12-28 15:21:21 -0300

    PROJECT-CHAT

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,9 +1,10 @@
-module.exports.home = (application, req, res)=>{
-    
-    req.assert('apelido', 'Apelido é obrigatório.').notEmpty();
-    
-    console.log(req.validationErrors);
-    
+module.exports.home = (application, req, res, errors)=>{
+    if(errors){
+        console.log(errors.array())
+        return res.render('index', {
+            error : errors.array()
+        });
+    }
     res.render('chat', {
         user : req.body.apelido
     });
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,5 +1,12 @@
+const { check, validationResult } = require('express-validator');
 module.exports = (application)=>{
-    application.post('/chat', (req, res)=>{
+    application.post('/chat', [
+        check('apelido').isLength({ min: 5 })
+    ], (req, res)=>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return application.app.controllers.chat.home(application, req, res, errors);;
+        }
         application.app.controllers.chat.home(application, req, res);
     });
-}// JavaScript Document
+}
